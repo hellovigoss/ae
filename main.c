6fa358d8c2a0f789b65cbfc161abba176bc14fe4
hellovigoss <hellovigoss@gmail.com>
2015-07-02 09:48:05 +0800

    update

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ aeFileProc *doRead(struct aeEventLoop *el, int client_sk, void *clientData, int 
 }
 
 aeFileProc *doAccept(struct aeEventLoop *el, int server_sk, void *clientData, int mask){
-    info("new connect");
+    info("accept");
     struct sockaddr_in client_addr;
     socklen_t length = sizeof(client_addr);
     int client_sk = accept(server_sk, (struct sockaddr_in*)&client_addr, &length);
@@ -43,41 +43,56 @@ aeFileProc *doAccept(struct aeEventLoop *el, int server_sk, void *clientData, in
     }
 }
 
-int main(int argc, char* argv[]){
+void forkWorker(aeEventLoop *el, int server_sk){
+    void *clientData;
+    clientData = NULL;
     int pid = fork();
     if(pid < 0){
         perror("fork");
     }
     else if(pid == 0){
-        int server_sk = 0;
-        void *clientData;
-        clientData = NULL;
-
-        struct sockaddr_in local_addr;
-        memset(&local_addr, 0, sizeof(local_addr));
-        local_addr.sin_family = AF_INET;
-        local_addr.sin_addr.s_addr = INADDR_ANY;
-        local_addr.sin_port = htons(8000);
-
-        if((server_sk = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-            perror("socket");
-        }
-
-        if(bind(server_sk, &local_addr, sizeof(struct sockaddr)) < 0){
-            perror("bind");
-        }
-        if(listen(server_sk, 1) < 0){
-            perror("listen");
-        }
-
-        aeEventLoop *el = aeCreateEventLoop(1024);
+        info("fork");
         aeCreateFileEvent(el, server_sk, AE_READABLE, doAccept, clientData);
         aeMain(el);
     }
+}
+
+int main(int argc, char* argv[]){
+    int i = 1;
+    aeEventLoop *el = aeCreateEventLoop(1024);
+
+    int server_sk = 0;
+
+    struct sockaddr_in local_addr;
+    memset(&local_addr, 0, sizeof(local_addr));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    local_addr.sin_port = htons(8000);
+
+    if((server_sk = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        perror("socket");
+    }
+
+    if(bind(server_sk, &local_addr, sizeof(struct sockaddr_in)) < 0){
+        perror("bind");
+    }
+
+    if(listen(server_sk, 1) < 0){
+        perror("listen");
+    }
+
+    /*aeCreateFileEvent(el, server_sk, AE_READABLE, doAccept, NULL);*/
+    forkWorker(el, server_sk);
+    forkWorker(el, server_sk);
+    forkWorker(el, server_sk);
+    forkWorker(el, server_sk);
+
+    pid_t pid = getpid();
 
     do{
         sleep(1);
     }while(waitpid(pid, NULL, WNOHANG) == 0);    
+
 
     return -1;
 }
